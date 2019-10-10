4737f49089004cb9d61ea62138cb42b5534c096d
Michele Piperni <michele.piperni1@gmail.com>
2019-10-10 17:06:23 -0600

    Header file

--- Lab1_BF609_Core0/src/Lab1_BF609_Core0.h
+++ Lab1_BF609_Core0/src/Lab1_BF609_Core0.h
@@ -13,26 +13,28 @@
 #include "../../ENCM511_SpecificFiles/ENCM511_include/REB_GPIO_Input_Library.h"
 #include "../../ENCM511_SpecificFiles/ENCM511_include/REB_GPIO_Output_Library.h"
 
-//Prototypes for Lab0
 
-
+//Initialization Prototypes
 void My_Init_LEDInterface(void);
-unsigned char My_ReadLED(void);
 void My_Init_SwitchInterface(void);
+void My_Init_GPIO_REB_Input(void);
+void My_Init_GPIO_REB_Output(void);
+
+//Read Prototypes
+unsigned char My_ReadLED(void);
 unsigned char My_ReadSwitches(void);
+
+//Write Prototypes
 void My_WriteLED(unsigned char);
+void My_Write_REB_LED(unsigned short int);
+
+//Other Prototypes for Lab 1
+void Start_Lab1(void);
 void charToBinary(unsigned char, unsigned char*);
 void WaitTillSwitch1PressedAndReleased(void);
 void WaitTillSwitch2PressedAndReleased(void);
 void WaitTillSwitch3PressedAndReleased(void);
 extern "C" unsigned long long int ReadProcessorCyclesASM(void);
-
-
-//Prototypes for Lab 1
-void Start_Lab1(void);
-void My_Init_GPIO_REB_Input(void);
-void My_Init_GPIO_REB_Output(void);
-void My_Write_REB_LED(unsigned short int);
 
 
 #endif /* __LAB1_BF609_CORE0_H__ */
