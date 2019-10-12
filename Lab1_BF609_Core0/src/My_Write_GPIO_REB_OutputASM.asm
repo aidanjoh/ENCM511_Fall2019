27d3d07221bb91e4e5c463b6d9f97d3a31727cee
aidanjoh <43867551+aidanjoh@users.noreply.github.com>
2019-10-12 15:18:56 -0600

    Adding comments to the code

--- Lab1_BF609_Core0/src/My_Write_GPIO_REB_OutputASM.asm
+++ Lab1_BF609_Core0/src/My_Write_GPIO_REB_OutputASM.asm
@@ -18,20 +18,22 @@
 _My_Write_GPIO_REB_OutputASM:
 	LINK 20;
 	
-	R1 = MaskBitValues11to0;
+	R1 = MaskBitValues11to0; //Storing the mask value into R1 register
+	
+	//This code is storing the value in the port F register into the pointer register P0
 	P0.L = lo(REG_PORTF_DATA);
 	P0.H = hi(REG_PORTF_DATA);
 	
-	R2 = W[P0](Z);
-	R3 = MaskBitValues11to0;
+	R2 = W[P0](Z); //Putting the value for the port F register into the R2 register (this is reading the values)
+	R3 = MaskBitValues11to0; //Putting the mask value into the register R3
 	
-	returnValue_R0 = returnValue_R0 << 12;
+	returnValue_R0 = returnValue_R0 << 12; //Shifting the value for the LEDs up to the output pins
 	
-	R2 = R2 & R3;
+	R2 = R2 & R3; //Masking the port F register
 	
-	returnValue_R0 = returnValue_R0 | R2;
+	returnValue_R0 = returnValue_R0 | R2; //Oring the port F register with the correct value for the LED outputs
 	
-	[P0] = returnValue_R0;
+	[P0] = returnValue_R0; //Storing theses new output values into the port F register so it will display the correct LED orientation
 	
 	UNLINK;
 
