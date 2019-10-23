4f72d92b79ddc84373825db301e9a26e10d13941
aidanjoh <43867551+aidanjoh@users.noreply.github.com>
2019-10-23 14:47:47 -0600

    My Stuff

---
+++ Coffeepot_Sim533/src/Coffeepot_Sim533.h
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Coffeepot_Sim533.h
+ *****************************************************************************/
+
+#ifndef __COFFEEPOT_SIM533_H__
+#define __COFFEEPOT_SIM533_H__
+
+/* Add your custom header content here */
+
+#endif /* __COFFEEPOT_SIM533_H__ */
