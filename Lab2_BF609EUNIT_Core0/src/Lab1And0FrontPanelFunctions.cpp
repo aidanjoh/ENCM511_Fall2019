0aa2d367dbe9ffe996b9e48ec464ce93e8b3388d
Michele Piperni <michele.piperni1@gmail.com>
2019-11-22 12:14:16 -0700

    Merge conflicts and add FP threads

---
+++ Lab2_BF609EUNIT_Core0/src/Lab1And0FrontPanelFunctions.cpp
@@ -0,0 +1,40 @@
+/*
+ * Lab1And0FrontPanelFunctions.cpp
+ *
+ *  Created on: Nov 21, 2019
+ *      Author: aidan
+ */
+#include "Lab1And0FrontPanelFunctions.h"
+
+void myWriteFrontPanelLEDs(unsigned char neededLEDValue) //This function is writing the values to be displayed by the LEDs
+{
+	if (My_Init_LEDInterface_Done == false)
+	{
+		return;
+	}
+
+	Write_GPIO_FrontPanelLEDS(neededLEDValue); //Writing the value to the panel of LEDs
+}
+
+unsigned char myReadFrontPanelSwitches(void) //This function is reading the switches from the panel
+{
+		if (My_Init_SwitchInterface_Done == false)
+		{
+			return GARBAGEVALUE;
+		}
+		FRONTPANEL_LED_8BIT_VALUE activeLowValues = Read_GPIO_FrontPanelSwitches();
+		FRONTPANEL_LED_8BIT_VALUE activeHighValues = ~activeLowValues;
+		FRONTPANEL_LED_8BIT_VALUE wantedSwitchValueActiveHigh = activeHighValues & MASK_KEEP_LOWER_FIVE_BITS;
+
+		return wantedSwitchValueActiveHigh;
+}
+
+unsigned char myReadFrontPanelLEDs(void)
+{
+	if (My_Init_LEDInterface_Done == false)
+	{
+		return GARBAGEVALUE;
+	}
+
+	return Read_GPIO_FrontPanelLEDS();
+}
